a8c1999a4869e39a10cd439fb348ee56e8515257
svasquez7 <svasquez7@gmail.com>
2018-07-24 15:53:30 -0600

    rewored and improved- possibly final Version for now

--- Time_Elapse_V1-0.py
+++ Time_Elapse_V1-0.py
@@ -12,13 +12,14 @@ from datetime import datetime
 
 camera = PiCamera()
 
-DEVICEID = "SVasquez-RaspberryPi3"
-PROJECTNAME = "Local Yocal 1"
-HUB_CONNECTION_STRING = "HostName=TimeElapseFun.azure-devices.net;DeviceId=SVasquez-RaspberryPi3;SharedAccessKey=LfgWJvx/RKd8MyebWRyQA6xOUiNGGrfmFDBjcZzFBJ4="
+DEVICEID = "My-RaspberryPi-3b"
+PROJECTNAME = "IOT Fun"
+HUB_CONNECTION_STRING = "HostName=TimeElapseFun.azure-devices.net;DeviceId=GitHubUser;SharedAccessKey=Y8TPJJKHobcN0ciOdmzxM5HbE+BxKUcyTIcywv0JCVw="
 BLOB_PROTOCOL = IoTHubTransportProvider.HTTP
 
 MESSAGE_PROTOCOL = IoTHubTransportProvider.MQTT
 MESSAGE_TIMEOUT = 10000
+ISSENDING = False
 
 PATHTOFILE = '/home/pi/Pictures/'
 FILENAME = datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
@@ -42,10 +43,20 @@ def take_picture():
         print ("an error occured capturing picture.")
 
 def send_confirmation_callback(message, result, user_context):
+    global ISSENDING
+    if str(result) == 'OK':
+        ISSENDING = False
+   
     print ( "IoT Hub responded to message with status: %s" % (result) )
+    print ( "ISSENDING: %s" % (ISSENDING) )
+
+def iothub_client_init(protocol):
+    # Create an IoT Hub client
+    client = IoTHubClient(HUB_CONNECTION_STRING, protocol)
+    return client
 
 def iothub_file_upload():    
-    client = IoTHubClient(HUB_CONNECTION_STRING, BLOB_PROTOCOL)
+    client = IoTHubClient(BLOB_PROTOCOL)
     f = open(PATHTOFILE + FILENAME, "rb")
     content = base64.b64encode(f.read())
     client.upload_blob_async(FILENAME, content, len(content), blob_upload_conf_callback, 0)
@@ -54,12 +65,18 @@ def iothub_file_upload():
 
 def iothub_client_post_message():
 
-    message_client = IoTHubClient(HUB_CONNECTION_STRING, MESSAGE_PROTOCOL)    
-    msg_txt_formatted = MSG_TXT % (DEVICEID, PROJECTNAME, FILENAME)
-    message = IoTHubMessage(msg_txt_formatted)
-    # Send the message.
-    print( "Sending message: %s" % message.get_string() )
-    message_client.send_event_async(message, send_confirmation_callback, None)
+    def iothub_client_post_message():
+    global ISSENDING
+    ISSENDING = True
+    while ISSENDING:
+        message_client = iothub_client_init(MESSAGE_PROTOCOL)    
+        msg_txt_formatted = MSG_TXT % (DEVICEID, PROJECTNAME, FILENAME)
+        message = IoTHubMessage(msg_txt_formatted)
+        # Send the message.
+        print( "Sending message: %s" % message.get_string() )
+        message_client.send_event_async(message, send_confirmation_callback, None)
+        time.sleep(7)
+
 
 
 if __name__ == '__main__':
